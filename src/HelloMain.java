90f02fbbf8a0a8f2558466d7c9b0118b9db18eb1
edwardmm03 <edwardmm@Clarkson.edu>
2022-10-04 12:12:32 -0400

    Practice Git First Commit

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -5,5 +5,6 @@ public class HelloMain
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("Practice project using github");
     }
 }
